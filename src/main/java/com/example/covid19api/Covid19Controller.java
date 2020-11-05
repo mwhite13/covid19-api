255dd16f843f6f6e853fb5605c0d6ac46ca56519
Mansoor White <mwhite13@ggc.edu>
2020-11-05 13:14:48 -0500

    It be like this sometimes

--- src/main/java/com/example/covid19api/Covid19Controller.java
+++ src/main/java/com/example/covid19api/Covid19Controller.java
@@ -12,21 +12,17 @@ import org.springframework.web.bind.annotation.*;
 public class Covid19Controller {
     @Autowired
     private Covid19Dao covid19Dao;
-
     public static WebDriver webDriver;
+
     public static final String COVID_URL ="https://coronavirus.jhu.edu/map.html";
     public static final String COVID19_NATIONALCASES = "//*[@id=\"ember1051\"]/div/div/h5/span[1]/strong";
     public static final String COVID19_NATIONALDEATH =" //*[@id=\"ember1435\"]/div/div/p[1]/span";
     public static final String COVID19_STATEDATA = "";
     public static final String COVID19_STATEDEATH ="";
 
-    public static void setup(){
-        System.setProperty("webDriver.chrome.driver","chromedriver.exe");
-
-    }
-
     @RequestMapping(value = "/addCases", method = RequestMethod.PUT)
     public Covidcase addCases(@RequestBody Covidcase c) throws Exception{
+        System.setProperty("","");
         covid19Dao.create(c);
         return c;
     }
