a66c836a2cbf9b4ab53d671d91790eda9f75d33c
jjuarez23 <51659338+jjuarez23@users.noreply.github.com>
2020-11-05 16:20:23 -0500

    PUT method works and records data to database
    Added a Test Class
    And implemented PUT method

--- src/main/java/com/example/covid19api/Covidcase.java
+++ src/main/java/com/example/covid19api/Covidcase.java
@@ -10,33 +10,41 @@ public class Covidcase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private int us_cases;
-    private int us_deaths;
-    private int ga_cases;
-    private int ga_deaths;
+    private String us_cases;
+    private String us_deaths;
+    private String ga_cases;
+    private String ga_deaths;
     private String date;
 
     public Covidcase() {}
+
+    public Covidcase(String usCases, String usDeaths, String gaCases, String gaDeaths, String date) {
+        this.us_cases = usCases;
+        this.us_deaths = usDeaths;
+        this.ga_cases = gaCases;
+        this.ga_deaths = gaDeaths;
+        this.date = date;
+    }
 
     public int getId() { return id; }
 
     public void setId(int id) { this.id = id; }
 
-    public int getUs_cases() { return us_cases; }
+    public String getUs_cases() { return us_cases; }
 
-    public void setUs_cases(int us_cases) { this.us_cases = us_cases; }
+    public void setUs_cases(String us_cases) { this.us_cases = us_cases; }
 
-    public int getUs_deaths() { return us_deaths; }
+    public String getUs_deaths() { return us_deaths; }
 
-    public void setUs_deaths(int us_deaths) { this.us_deaths = us_deaths; }
+    public void setUs_deaths(String us_deaths) { this.us_deaths = us_deaths; }
 
-    public int getGa_cases() { return ga_cases; }
+    public String getGa_cases() { return ga_cases; }
 
-    public void setGa_cases(int ga_cases) { this.ga_cases = ga_cases; }
+    public void setGa_cases(String ga_cases) { this.ga_cases = ga_cases; }
 
-    public int getGa_deaths() { return ga_deaths; }
+    public String getGa_deaths() { return ga_deaths; }
 
-    public void setGa_deaths(int ga_deaths) { this.ga_deaths = ga_deaths; }
+    public void setGa_deaths(String ga_deaths) { this.ga_deaths = ga_deaths; }
 
     public String getDate() { return date; }
 
--- src/main/java/com/example/covid19api/TestGetCovidData.java
+++ src/main/java/com/example/covid19api/TestGetCovidData.java
@@ -4,39 +4,26 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.testng.annotations.BeforeMethod;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+public class TestGetCovidData {
 
-@RestController
-
-public class Covid19Controller {
-    @Autowired
-    private Covid19Dao covid19Dao;
     public static WebDriver webDriver;
-
-    public static final String COVID_USA_URL ="https://coronavirus.jhu.edu/region/united-states";
+    public static final String COVID_URL ="https://coronavirus.jhu.edu/region/united-states";
     public static final String COVID_GA_URL = "https://coronavirus.jhu.edu/region/us/georgia";
 
-    @RequestMapping(value = "/addCases", method = RequestMethod.PUT)
-    public Covidcase addCases() throws Exception{
+    @Test
+    public void testGetCovidNumbers() {
         System.setProperty("webdriver.chrome.driver", "/Users/juanjuarez/Desktop/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
         webDriver = new ChromeDriver(chromeOptions);
-        webDriver.get(COVID_USA_URL);
-        // webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriver.get(COVID_URL);
+       // webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         String usCases = webDriver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div[1]/div/div/div[2]/div[1]/span")).getText();
         System.out.println("US cases = " + usCases);
         String usDeaths = webDriver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div[1]/div/div/div[2]/div[2]/span")).getText();
@@ -47,25 +34,5 @@ public class Covid19Controller {
         String gaDeaths = webDriver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div[2]/div[2]/span")).getText();
         System.out.println("GA deaths = " + gaDeaths);
 
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
-        Date date = new Date();
-
-        Covidcase newCase = new Covidcase(usCases,usDeaths,gaCases,gaDeaths, formatter.format(date));
-        covid19Dao.create(newCase);
-
-        webDriver.close();
-
-        return newCase;
-
-    }
-
-    @RequestMapping(value = "/getCases/{id}" , method = RequestMethod.GET)
-    public Covidcase getCases(@PathVariable("id")int id){
-        return null;
-    }
-
-    @RequestMapping(value = "/deleteCaseData/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> deleteCaseData(@PathVariable("id")int id){
-        return null;
     }
 }
