846f3c525522c34ad1fe442d932ae7f8be862146
Mansoor White <mwhite13@ggc.edu>
2020-11-05 13:08:40 -0500

    Update Methods

--- src/main/java/com/example/covid19api/Covid19Dao.java
+++ src/main/java/com/example/covid19api/Covid19Dao.java
@@ -1,4 +1,29 @@
 package com.example.covid19api;
 
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public class Covid19Dao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public void create(Covidcase covidcase){
+        entityManager.persist(covidcase);
+        return;
+    }
+    public Covidcase getCaseId(int id){
+        return entityManager.find(Covidcase.class,id);
+    }
+
+    public Covidcase deleteCase(int id){
+        Covidcase covidcase = entityManager.find(Covidcase.class, id);
+        entityManager.remove(covidcase);
+        return covidcase;
+    }
 }
